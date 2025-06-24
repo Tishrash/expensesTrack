2cc06b52eaed7841aaad40d1583d620365e60632
M.G.T Rashmika <99706758+Tishrash@users.noreply.github.com>
2025-06-24 22:03:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
+const repo = 'expensesTrack';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Add basePath if your site is not at the root of the domain
-  // basePath: '/expensesTrack',
-  // Add trailingSlash for GitHub Pages compatibility
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   trailingSlash: true,
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
